30f7416eb3465a0dcc610b8cdd8c45cb19305cd7
giuem <giuemcom@gmail.com>
2019-07-31 15:23:50 +0800

    feat: use @giuem/builder

--- rollup.config.js
+++ rollup.config.js
@@ -1,69 +1,7 @@
+import builder from "@giuem/builder";
+
 import babel from "rollup-plugin-babel";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import json from "rollup-plugin-json";
-import sourceMaps from "rollup-plugin-sourcemaps";
-import camelcase from "camelcase";
 
-import pkg from "./package.json";
-
-const extensions = [".js" /* ".jsx", ".ts", ".tsx" */];
-
-function getPkgName() {
-  const matched = /^(?:@[^/@]+\/)?([^/@]+)/.exec(pkg.name);
-  const rawPkgName = matched[1];
-  if (!rawPkgName) {
-    throw new URIError(`Failed to parse package.name: ${pkg.name}`);
-  }
-  return camelcase(rawPkgName);
-}
-
-// const isProd = process.env.NODE_ENV === "production";
-
-/** @type {import("rollup").Plugin[]} */
-const commonPlugins = [
-  resolve({ extensions }),
-
-  sourceMaps(),
-
-  json(),
-
-  commonjs({
-    include: "node_modules/**",
-    extensions,
-    ignoreGlobal: true
-  }),
-
-  babel({ extensions, include: ["src/**/*"] })
-];
-
-/** @type {import("rollup").RollupOptions} */
-const baseConfig = {
-  input: "src/index.js",
-  plugins: commonPlugins
-};
-
-const CjsConfig = Object.assign({}, baseConfig, {
-  output: {
-    // exports: "named",
-    file: pkg.main,
-    format: "cjs"
-  }
+export default builder({
+  plugins: [babel()]
 });
-
-const EsmConfig = Object.assign({}, baseConfig, {
-  output: {
-    file: pkg.module,
-    format: "esm"
-  }
-});
-
-const BrowserConfig = Object.assign({}, baseConfig, {
-  output: {
-    name: getPkgName(),
-    file: pkg.browser,
-    format: "iife"
-  }
-});
-
-export default [CjsConfig, EsmConfig, BrowserConfig];
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const CONSTANT = "constant";
 
+export { default as json } from "./test.json";
 export { default as add } from "./add";
 export { default as minus } from "./minus";
 export { CONSTANT };
